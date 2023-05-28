c5d3c719fe035571cc02a28e8d0ad3df1ff7aa66
Cristian3092 <easyroad4@gmail.com>
2023-05-28 19:06:46 +0200

    Center calculator & clean code
    Se realiza un centrado de la calculadora en pagina y además se corrigen codigo en main.js

--- calculadora.js
+++ calculadora.js
@@ -1,20 +1,20 @@
 function agregar(caracter) {
-    document.getElementById('resultado').value += caracter;
+    document.getElementById('resultado').innerHTML += caracter;
 }
 
 
 function limpiar() {
-    document.getElementById('resultado').value = '';
+    document.getElementById('resultado').innerHTML = '';
 }
 
 
 function borrar() {
-    let resultado = document.getElementById('resultado').value;
-    document.getElementById('resultado').value = resultado.slice(0, -1);
+    let resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.slice(0, -1);
 }
 
 function calcular() {
-    let resultado = document.getElementById('resultado').value;
+    let resultado = document.getElementById('resultado').innerHTML;
     let total = eval(resultado);
-    document.getElementById('resultado').value = total;
+    document.getElementById('resultado').innerHTML = total;
 }
